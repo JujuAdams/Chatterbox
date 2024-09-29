c392ebd68b962ae67f4bd533078a988aafeaf7c6
Juju Adams <julian.adams@email.com>
2024-09-29 21:19:40 +0100

    Updates Docsify implementation

--- docs/_sidebar.md
+++ docs/_sidebar.md
@@ -1,19 +1,27 @@
 -   [Introduction](README)
+-   [Alternatives](Alternatives)
+-   [Getting Started](getting-started)
+-   [Spider Lily's Tutorial](https://spiderlilystudios.medium.com/clickable-choices-in-gms2-using-chatterbox-and-scribble-0ed81dfe16a5)
 
 ---
 
 **Concepts**
 
--   [Yarn Script](concept-yarn-script)
+-   [YarnScript](concept-yarn-script)
 -   [Source Files](concept-source-files)
 -   [Chatterboxes](concept-chatterboxes)
+-   [Metadata and Speakers](concept-metadata-and-speakers)
+-   [Actions and Functions](concept-actions-functions)
 
 ---
 
 **API Reference**
 
 -   [Configuration](reference-configuration)
+-   [Setup](reference-setup)
 -   [Chatterboxes](reference-chatterboxes)
--   [Getters](reference-getters)
--   [Flow](reference-flow)
--   [Variables](reference-variables)
+-   [Content Getters](reference-content-getters)
+-   [Option Getters](reference-option-getters)
+-   [Flow Control](reference-flow)
+-   [Variables and Constants](reference-variables)
+-   [Localisation](reference-localisation)
---
+++ docs/config.js
@@ -0,0 +1,29 @@
+/*
+	UPDATING VERSIONS:
+	1. Just change the `latestVersion` variable to the new version.
+	2. Put the old version into the `otherVersions` array.
+	
+	PREVIEWING CHANGES:
+	-	If you'd like to make future modifications to markdown files and preview them before pushing your changes,
+		you can do so with either Python or Node. If you have Node installed you can install Docsify with
+		'npm i docsify-cli -g' and then navigate to the docs folder and run 'docsify serve'. With Python you
+		can manually serve your docs with 'cd docs && python -m http.server 3000'.
+		More info: https://docsify.js.org/#/quickstart
+	
+	OTHER THINGS OF NOTE:
+	-	If you want a different sidebar or navbar for different versions,
+		you can just copy and paste the _sidebar.md or _navbar.md from the
+		main /docs/ folder into the version folder you'd like and modify it there.
+	
+	-	This should really be the only file you'll need to mess with, if any other changes are needed
+		please let @FaultyFunctions know!
+*/
+
+const config = {
+	name: 'Chatterbox',
+	description: 'Narrative design toolkit for GameMaker',
+	latestVersion: '2.16',
+	otherVersions: ['2.17', '2.15', '2.14', '2.13', '2.12', '2.11', '2.10', '2.9', '2.8', '2.7', '2.6', '2.5', '2.4', '2.3', '2.2', '2.1', '1.1'],
+	favicon: 'assets/favicon.ico',
+	themeColor: '#f0Bc00',
+};
